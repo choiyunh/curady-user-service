9dba2a29c670e70f0c011139f0b3d3a1fd9d7a0d
YunHo Choi <svs101@dgu.ac.kr>
2022-09-13 18:50:22 +0900

    Feat/#9: 회원 정보 수정 API
    - 성향 초기 데이터 생성

---
+++ src/main/resources/initdata/curady_user/data.sql
@@ -0,0 +1,23 @@
+INSERT INTO tendency (type, name) values ('lecture', 'theory');
+INSERT INTO tendency (type, name) values ('lecture', 'practice');
+INSERT INTO tendency (type, name) values ('lecture', 'easy');
+INSERT INTO tendency (type, name) values ('lecture', 'clear');
+
+INSERT INTO tendency (type, name) values ('tech', 'java');
+INSERT INTO tendency (type, name) values ('tech', 'python');
+INSERT INTO tendency (type, name) values ('tech', 'html');
+INSERT INTO tendency (type, name) values ('tech', 'css');
+INSERT INTO tendency (type, name) values ('tech', 'javascript');
+INSERT INTO tendency (type, name) values ('tech', 'c/c++');
+
+INSERT INTO tendency (type, name) values ('level', '0');
+INSERT INTO tendency (type, name) values ('level', '1');
+INSERT INTO tendency (type, name) values ('level', '2');
+INSERT INTO tendency (type, name) values ('level', '3');
+INSERT INTO tendency (type, name) values ('level', '4');
+INSERT INTO tendency (type, name) values ('level', '5');
+
+INSERT INTO tendency (type, name) values ('price', '0');
+INSERT INTO tendency (type, name) values ('price', '1');
+INSERT INTO tendency (type, name) values ('price', '2');
+INSERT INTO tendency (type, name) values ('price', '3');
